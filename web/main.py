7ad6ef91521eb980d12da56c2220bfd22caa4dd3
Liang Sun <i@liangsun.org>
2012-04-10 22:11:01 +0800

    Work on search page.

--- web/main.py
+++ web/main.py
@@ -21,7 +21,15 @@ class MainHandler(tornado.web.RequestHandler):
 
 class SearchHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Your query is " + self.get_argument("query"))
+        items = [{"title":"上海租房网,上海租房信息,上海租房信息, 租房子,搜屋网。",
+            "url":"http://www.url.com",
+            "price":"823元/月",
+            "area":"80平米",
+            "style":"2室1厅1卫",
+            "address":"海淀区学院南路东三元胡同",
+            "district":"屯三里小区"}] * 10
+        self.render("search.html", query=self.get_argument("query"), items=items)
+        #self.write("Your query is " + self.get_argument("query"))
         
 def main():
     tornado.options.parse_command_line()
