ef7e86247c1ab1e22674c89b07faa54d7e4d1504
Liang Sun <i@liangsun.org>
2012-04-14 17:31:08 +0800

    Work on bot.

--- bot/bot.py
+++ bot/bot.py
@@ -3,33 +3,45 @@
 
 import re
 import sys
+import codecs
 import urllib
 import robotexclusionrulesparser
+from tornado.database import Connection
 
 g_user_agent = 'Zfz-bot/1.0'
 
-g_link_pattern = re.compile(r'<a\s+[^>]*href=[\'"]?([^\s\'">]+)[\'"]?', re.U | re.I)
-g_price_pattern = re.compile(ur'租金：<em>(\d+)</em><span>元/月', re.U | re.I)
-g_address_pattern = re.compile(ur'地址：<[^>]+>([^<]+)<', re.U | re.I)
+g_link_pattern = re.compile(r'\s+href="([^\s\'">]+)"[\s>]', re.U | re.I)
+#href="http://beijing.anjuke.com/prop/rent/107883327"
+g_price_pattern = re.compile(ur'租金：[^\d]*(\d+)(<[^<>]+>)*元/月', re.U | re.I)
 g_area_pattern = re.compile(ur'面积：(\d+)平米', re.U | re.I)
 g_arch_pattern = re.compile(ur'房型：([^\s<]+)<', re.U | re.I)
 g_title_pattern = re.compile(ur'<title>([^<]+)</title>', re.U | re.I)
+g_address_pattern = re.compile(ur'地址：<[^>]+>([^<]+)<', re.U | re.I)
 g_district_pattern = re.compile(ur'小区：<[^>]+>([^<]+)<', re.U | re.I)
 
 g_max_url_length = 200
+
 g_max_price_length = 10
-g_max_address_length = 100
 g_max_area_length = 10
 g_max_arch_length = 20
 g_max_title_length = 100
+g_max_address_length = 100
 g_max_district_length = 20
 
-g_page_limit = 3
+g_page_limit = 10 ** 6
+
+g_db = Connection('127.0.0.1', 'zfz', 'zfz', 'zfz...891')
 
 class ZfzURLopener(urllib.FancyURLopener):
     version = g_user_agent
 
 urllib._urlopener = ZfzURLopener()
+
+def is_valid_url(url):
+    #print url
+    ans = re.match(ur'http://beijing.anjuke.com/prop/rent/\d+', url) != None
+    #print ans
+    return ans
 
 def get_page(url):
     global g_page_limit
@@ -38,58 +50,72 @@ def get_page(url):
         sys.exit(0)
       
     print 'get page:' + url
-    
+
+   
     if not url.startswith('http://'):
         return None
 
     try:
-        ans = urllib.urlopen(url).read()
+        ans = urllib.urlopen(url).read().decode('utf-8')
     except:
         return None
 
+    f = codecs.open('tmp', 'w', 'utf-8')
+    f.write(ans)
+    f.close()
+ 
     return ans
 
 def get_all_links(page):
     return g_link_pattern.findall(page)
 
 def add_result_to_db(url, result):
-    print 'Adding %s to db.' % url
+    print '++++++Adding %s to db.' % url
     #price, address, area, arch, title, district = result
-    print result
+    for i in range(len(result)):
+        result[i] = result[i].encode('utf-8')
+        print result[i]
+
+    title, price, area, arch, address, district = result
+
+    g_db.execute("insert ignore into pages (url, price, address, area, arch, title, district) values (%s, %s, %s, %s, %s, %s, %s)",
+    url, int(price), address, int(float(area) * 100), arch, title, district)
+
+
 
 def analyse(page):
     m = g_price_pattern.search(page)
-    if m == None:
+    if m == None or len(m.group(1)) > g_max_price_length:
         print 'No price'
         return None
     price = m.group(1)
     
     m = g_address_pattern.search(page)
-    if m == None:
+    if m == None or len(m.group(1)) > g_max_address_length:
         print 'No address'
         return None
     address = m.group(1)
 
     m = g_area_pattern.search(page)
-    if m == None:
+    if m == None or len(m.group(1)) > g_max_area_length:
         print 'No area'
         return None
     area = m.group(1)
 
     m = g_arch_pattern.search(page)
-    if m == None:
+    if m == None or len(m.group(1)) > g_max_arch_length:
         print 'No arch'
         return None
     arch = m.group(1)
 
     m = g_title_pattern.search(page)
-    if m == None:
+    if m == None or len(m.group(1)) > g_max_title_length:
         print 'No title'
         return None
     title = m.group(1)
 
     m = g_district_pattern.search(page)
-    if m == None:
+    if m == None or len(m.group(1)) > g_max_district_length:
         print 'No district'
         return None
     district = m.group(1)
@@ -129,10 +155,11 @@ def crawl_web(root):
             continue
 
         links = get_all_links(page)
+        #print links
         for link in links:
             #Only search insite page in this version
-            if link.startswith('http') and not link.startswith(root):
-                continue
+            #if link.startswith('http') and not link.startswith(root):
+            #    continue
 
             #Deal with absolute path and relative path
             if link.startswith('/'):
@@ -151,7 +178,8 @@ def crawl_web(root):
                 continue
 
             if full_link not in crawled:
-                tocrawl.add(full_link)
+                if is_valid_url(full_link):
+                    tocrawl.add(full_link)
 
         add_page_to_index(url, page)
 
@@ -162,6 +190,9 @@ def crawl_web(root):
 #        'http://haozu.com']
 #
 #seeds = ['http://beijing.anjuke.com']
-seeds = ['http://beijing.anjuke.com/prop/rent/107862213']
+seeds = ['http://beijing.anjuke.com/rental/']
 for seed in seeds:
     crawl_web(seed)
+
+
+g_db.close()
