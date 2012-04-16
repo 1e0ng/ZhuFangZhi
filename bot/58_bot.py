b3c46a6679a5d94620b7bb9831038b20fe519049
Liang Sun <i@liangsun.org>
2012-04-16 23:23:04 +0800

    Make 58, anjuke, haozu crawl cities all over China.

--- bot/58_bot.py
+++ bot/58_bot.py
@@ -6,10 +6,10 @@ from bot import Robot
 class Robot58(Robot):
     def __init__(self, root, charset):
         Robot.__init__(self, root, charset)
-        self.url_pattern = re.compile(ur'http://bj.58.com/zufang/.*', re.U | re.I)
+        self.url_pattern = re.compile(ur'http://\w+\.58\.com/zufang/.*', re.U | re.I)
 
     def is_valid_url(self, url):
         return Robot.is_valid_url(self, url) and self.url_pattern.match(url) != None
 
-a = Robot58('http://bj.58.com/zufang/', 'utf-8')
+a = Robot58('http://www.58.com/zufang/changecity/', 'utf-8')
 a.crawl_web()
--- bot/haozu_bot.py
+++ bot/haozu_bot.py
@@ -6,7 +6,7 @@ from bot import Robot
 class RobotHaozu(Robot):
     def __init__(self, root, charset):
         Robot.__init__(self, root, charset)
-        self.url_pattern = re.compile(ur'http://beijing.haozu.com/(listing/p%d+|rental/broker/n/\d+)/?')
+        self.url_pattern = re.compile(ur'http://\w+\.haozu\.com/(listing/(p%d+)?|rental/broker/n/\d+)?/?$')
 
     def is_valid_url(self, url):
         return Robot.is_valid_url(self, url) and self.url_pattern.match(url) != None
